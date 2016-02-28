9831c506414e9afb41b80f93b67a751486aa89de
Alex Gofman <gofmanalex13@gmail.com>
2016-02-28 21:37:27 +0400

    set up webpack and index.html

--- webpack.config.js
+++ webpack.config.js
@@ -6,7 +6,18 @@ module.exports = {
   },
   devServer: {
     inline: true,
-    port: 333
-
+    port: 3333
+  },
+  module: {
+    loaders: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        loader: 'babel',
+        query: {
+          presets: ['es2015', 'react']
+        }
+      }
+    ]
   }
-}
+};
